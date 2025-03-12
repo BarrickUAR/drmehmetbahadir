faa53633867b291eca89c840bb3b79dccdf1a7a3
unknown <berkucar0901@gmail.com>
2025-03-12 12:57:41 +0300

    .

--- mail.php
+++ mail.php
@@ -1,39 +1,34 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $date = $_POST["date"];
+    $name = $_POST["name"] ?? '';
+    $email = $_POST["email"] ?? '';
+    $date = $_POST["date"] ?? '';
+    $address = $_POST["address"] ?? '';
+    $message = $_POST["message"] ?? '';
 
-    $to = "info@drmehmetilteberbahadir.net"; // Alıcı mail adresi
-    $subject = "Yeni Randevu Talebi";
-    $message = "Ad: $name\nE-posta: $email\nRandevu Tarihi: $date";
-    $headers = "From: $email" . "\r\n" . "Reply-To: $email";
-
-    if (mail($to, $subject, $message, $headers)) {
-        echo "success";
-    } else {
+    // En azından bir formdaki temel alanların dolu olup olmadığını kontrol edelim
+    if (empty($name) || empty($email)) {
         echo "error";
+        exit;
     }
-}
-?>
-
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $address = $_POST["address"];
-    $message = $_POST["message"];
 
     $to = "info@drmehmetilteberbahadir.net";
     $subject = "Yeni Randevu Talebi";
-    $body = "İsim: $name\nE-posta: $email\nAdres: $address\nMesaj: $message";
-    $headers = "From: $email";
 
-    if (mail($to, $subject, $body, $headers)) {
+    // Form hangi sayfadan geldiyse ona göre mesaj oluştur
+    if (!empty($date)) {
+        $email_message = "Ad: $name\nE-posta: $email\nRandevu Tarihi: $date";
+    } else {
+        $email_message = "Ad: $name\nE-posta: $email\nAdres: $address\nMesaj: $message";
+    }
+
+    $headers = "From: no-reply@seninsiten.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if (mail($to, $subject, $email_message, $headers)) {
         echo "success";
     } else {
         echo "error";
     }
 }
-?>a

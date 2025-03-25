f1257e9b088a1ade0f5530cf649eb52c6a872084
unknown <berkucar0901@gmail.com>
2025-03-25 14:37:15 +0300

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -678,7 +678,7 @@ function getCookie(e) {
           English: "en",
           Arabic: "ar",
           Russian: "ru", // "Russian" kelimesini "ru" dil koduna çevir
-          Deutsch: "de", // "Russian" kelimesini "ru" dil koduna çevir
+          Deutsch: "de", // 
         }[e] || "tr"
       );
     }
